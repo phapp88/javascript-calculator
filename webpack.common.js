ef565f12adbe9f046afe61df4a4f51c0d2af8ab9
Paul <phapp1988@gmail.com>
2018-03-19 20:57:27 -0500

    Add mobile styles

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,9 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
 
 const config = {
-  devtool: 'cheap-module-source-map',
-  entry: './index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -29,15 +17,10 @@ const config = {
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: './src/index.html',
       title: 'JavaScript Calculator',
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
   ],
-  devServer: {
-    hot: true,
-  },
 };
 
 module.exports = config;
